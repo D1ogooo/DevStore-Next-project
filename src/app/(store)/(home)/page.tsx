8af38db4b129850baefb4ca981c8dec6053b7433
D1ogooo <diiogocosta11@gmail.com>
2024-06-30 04:13:59 -0300

    Renderização de slugs completo.

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -5,14 +5,12 @@ import Link from "next/link";
 
 async function getFeaturedProducts(): Promise<Product[]> {
   const res = await api('/products/featured', {
-   next: {
-    revalidate: 60 * 60, // 1 hora
-   }
-  })
-
-  const products = await res.json()
-
-  return products
+    next: {
+      revalidate: 60 * 60, // 1 hora
+    }
+  });
+  const products = await res.json();
+  return products;
 }
 
 export default async function Home() {
@@ -34,37 +32,43 @@ export default async function Home() {
           <span className="text-sm truncate">{highlightedProduct.title}</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
             {highlightedProduct.price.toLocaleString('pt-BR', {
-             style: 'currency',
-             currency: 'BRL',
-             minimumFractionDigits: 0,
-             maximumFractionDigits: 0,
+              style: 'currency',
+              currency: 'BRL',
+              minimumFractionDigits: 0,
+              maximumFractionDigits: 0,
             })}
           </span>
         </div>
       </Link>
 
-      {otherProducts.map(product => {
+      {otherProducts.map((product) => {
         return (
           <Link
-          href={"/"}
-          className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-center"
-        >
-          <Image
-            src={product.image}
-            alt="Moletom Java"
-            className="group-hover:scale-105 transition-transform duration-500"
-            width={920}
-            height={920}
-            quality={100}
-          />
-          <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[17.5rem] rounded-full border-2 border-zing-500 bg-black/60 p-1 pl-5">
-            <span className="text-sm truncate">{product.title}</span>
-            <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-              {product.price}
-            </span>
-          </div>
-        </Link>
-        )
+            key={product.slug}
+            href={`/product/${product.slug}`}
+            className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-center"
+          >
+            <Image
+              src={product.image}
+              alt={product.title}
+              className="group-hover:scale-105 transition-transform duration-500"
+              width={920}
+              height={920}
+              quality={100}
+            />
+            <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[17.5rem] rounded-full border-2 border-zing-500 bg-black/60 p-1 pl-5">
+              <span className="text-sm truncate">{product.title}</span>
+              <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+                {product.price.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                  minimumFractionDigits: 0,
+                  maximumFractionDigits: 0,
+                })}
+              </span>
+            </div>
+          </Link>
+        );
       })}
     </div>
   );
--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,12 +1,32 @@
+import { api } from "@/data/api";
+import { Product } from "@/data/types/product";
 import Image from "next/image";
 
-export default function ProductPage() {
+interface ProductProps {
+ params: {
+  slug: string
+ }
+}
+
+async function getProduct(slug: string): Promise<Product> {
+  const res = await api(`/products/${slug}`, {
+   next: {
+    revalidate: 60 * 60, // 1 hora
+   }
+  })
+  const products = await res.json()
+  return products
+}
+
+export default async function ProductPage({ params }: ProductProps) {
+  const products = await getProduct(params.slug)
+
   return (
     <div className="relative grid max-h-[860px] grid-cols-3 text-white">
       <div className="col-span-2 overflow-hidden">
         <Image
           className="relative"
-          src="/moletom-never-stop-learning.png"
+          src={products.image}
           alt="Moletom Never Stop Learning"
           width={1000}
           height={1000}
@@ -15,46 +35,39 @@ export default function ProductPage() {
       </div>
       <div className="flex flex-col justify-center px-12">
         <h1 className="text-3xl font-bold leading-tight">
-          Moletom Never Stop Learning
+         {products.title}
         </h1>
         <p className="mt-2 leading-relaxed text-zinc-400">
-          Moletom fabricado com 88% de algodão e 12% de poliéster.
+         {products.description}
         </p>
         <div className="mt-8 flex items-center gap-3">
-          <span className="flex items-center rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
-            R$129
-          </span>
-          <span className="text-sm text-zinc-400">
-            Em 12x s/ juros de R$10,75
-          </span>
+         <span className="flex items-center rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
+         {products.price.toLocaleString('pt-BR', {
+             style: 'currency',
+             currency: 'BRL',
+             minimumFractionDigits: 0,
+             maximumFractionDigits: 0,
+            })}
+         </span>
+         <span className="text-sm text-zinc-400">
+          {(products.price / 12).toLocaleString('pt-BR', {
+           style: 'currency',
+           currency: 'BRL',
+          })}
+         </span>
         </div>
         <div className="mt-8 space-y-4">
           <span className="block font-semibold">Tamanhos</span>
           <div className="flex gap-2">
-            <button
-              type="button"
-              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
-            >
-              P
-            </button>
-            <button
-              type="button"
-              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
-            >
-              M
-            </button>
-            <button
-              type="button"
-              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
-            >
-              G
-            </button>
-            <button
-              type="button"
-              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
-            >
-              GG
-            </button>
+            {['P', 'M', 'G', 'GG'].map((size) => (
+              <button
+                key={size}
+                type="button"
+                className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+              >
+                {size}
+              </button>
+            ))}
           </div>
         </div>
 
